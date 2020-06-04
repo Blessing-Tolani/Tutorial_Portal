82ace5e00515caee496389292d69580098a75840
BlessingTolani <65096374+Blessing-Tolani@users.noreply.github.com>
2020-06-04 20:54:43 +0100

    Designed a Login and Register page

---
+++ peakt.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#signup-btn").on('click', showSignup);
+    $("#signup-form").on('submit', validate);
+  });
+function showSignup (){
+    $("#item31").hide();
+    $("#item32").show();
+}
+function hideSignup (){
+    $("#item31").show();
+    $("#item32").hide();
+ }
+ function validate (e) {
+    e.preventDefault();
+    var pass = $("#passcode").val();
+    var confpass = $("#passcode2").val();
+  
+    if (pass !== confpass) {
+      alert("Passcode don't match: Kindly re-enter the Confirm Passcode");
+      return false;
+    }
+ 
+    $("#login-form").append("<div id='countdown'>Account successfully created, redirecting to Login page in <b id='redirect-count'>5</b></div>");
+    var count = 5;
+    var timer = setInterval(function () {
+        count--;
+        $('#redirect-count').html(count);
+    }, 1000);
+
+    setTimeout(function () {
+        clearInterval(timer);
+        hideSignup();
+        $('#countdown').remove();
+    }, 5000);
+ }
