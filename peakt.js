68b058079557b9f15962178523d7e46a90e5bada
BlessingTolani <65096374+Blessing-Tolani@users.noreply.github.com>
2020-06-08 01:43:20 +0100

    Changed font style and size

--- peakt.js
+++ peakt.js
@@ -3,8 +3,21 @@ const navlinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener('click', () => {
+  //Toggle Nav
   navlinks.classList.toggle("open");
- 
+  //Animate links
+  links.forEach((link,index) => {
+    if(link.style.animation){
+      link.style.animation=''
+    }
+    else{
+      link.style.animation = `navLinkFade 2s ease forwards ${index / 7}s`
+    }
+  
+});
+  //burger animation
+  hamburger.classList.toggle("bum");
+
 });
 
 $(document).ready(function () {
