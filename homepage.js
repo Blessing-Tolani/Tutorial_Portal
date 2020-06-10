f150489d6bcf6249df0ad05a30dd647355c7bd61
BlessingTolani <65096374+Blessing-Tolani@users.noreply.github.com>
2020-06-10 01:59:27 +0100

    Finished designing the home page

---
+++ homepage.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Toggle link
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+          //Animate link
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+           link.style.animation='';
+        }
+        else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+       
+        console.log(index / 7);
+    });
+     //burger animation
+      burger.classList.toggle("bum");
+
+  
+    });
+}
+navSlide();
